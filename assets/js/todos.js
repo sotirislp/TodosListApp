7e11e2df2ce9a6525f5204dce4ad8a5e8f02dd49
Sotiris <slpetridis@gmail.com>
2018-09-24 09:15:05 +0300

    first commit

---
+++ assets/js/todos.js
@@ -0,0 +1,23 @@
+//Check Off Specific Todos By Clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val(); 		//grabbing new todo text from input
+		$(this).val("");			//erase the typed todo.
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todoText + "</li>");	//put the new todo in the list
+	}
+});
+
+$("#plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
